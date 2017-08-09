008315ccebe088e87af2215bd03a53bdf96fabc0
Chris Chapman <Chapman@apextion.com>
2017-08-09 13:04:07 -0400

    base controls in place

--- services/clients/restful-bridge/routes/ui/index.js
+++ services/clients/restful-bridge/routes/ui/index.js
@@ -11,7 +11,31 @@ module.exports = () => {
 
 
   router.get('/', (req, res) => {
-    res.render('dashboard');
+    const payload = {
+      menus: [{
+        title: 'General',
+        items: [
+          { title:'Dashboard', icon: 'md-view-dashboard', link: 'javascript:void(0)' },
+          { title:'Settings', icon: 'md-view-dashboard', link: 'javascript:void(0)' },
+        ]
+      }],
+      gauges: [
+        {title: 'Battery', context: 'progress-bar-warning', value: '75%'}
+      ],
+      listGroups: [{
+        title: 'Playlists',
+        items: [
+            { title:'None', icon: 'md-view-dashboard', link: 'javascript:void(0)', active: true },
+            // No Playlists defined yet
+            // TODO Define and config playlists
+            // { title:'Rainbow', icon: 'md-link', link: 'javascript:void(0)', active: false },
+            // { title:'Blue Gate', icon: 'md-view-dashboard', link: 'javascript:void(0)', active: false },
+            // { title:'Franks', icon: 'md-link', link: 'javascript:void(0)', active: false },
+          ]
+        }]
+      }
+
+    res.render('dashboard', payload);
   });
 
   return router;
